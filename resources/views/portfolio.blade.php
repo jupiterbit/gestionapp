7ba8f8e2f6bc3ef11ac15f799e8301dd89030d71
jupiterbit <ejoelhuillca@gmail.com>
2019-12-20 01:00:55 -0500

    proyecto

---
+++ resources/views/portfolio.blade.php
@@ -0,0 +1,15 @@
+@extends('layout')
+
+@section('title','Portfolio')
+
+@section('content')
+<h1>Portfolio</h1> 
+<ul>   
+    @forelse ($portfolio as $item)
+        <li>{{ $item['title'] }} <pre> {{var_dump($loop)}} </pre></li>
+    @empty
+        <li>Sin Proyectos</li>
+    @endforelse   
+</ul>
+  
+@endsection
---
+++ routes/web.php
@@ -0,0 +1,45 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+$portfolio=[    
+    ['title'=>'Proyecto #1'],
+    ['title'=>'Proyecto #2'],
+    ['title'=>'Proyecto #3'],
+    ['title'=>'Proyecto #4'],    
+];
+
+Route::view('/','home')->name('home');
+Route::view('/about','about')->name('about');
+Route::view('/portfolio','portfolio',compact('portfolio'))->name('portfolio');
+Route::view('/contact','contact')->name('contact');
+
+
+Route::get('alpha', function () {
+    $nombre="Edson";
+    //return view('home')->with('nombre',$nombre);
+    //return view('home')->with(['nombre'=>$nombre]);
+    return view('home', ['nombre'=>$nombre]);
+    //return view('home', compact('nombre'));
+})->name('inicio');
+
+Route::get('/contacto', function () {
+    return "Hola desde contacto";
+});
+
+Route::get('/contacto/{nombre}', function ($nombre) {
+    return "Hola $nombre";
+});
+
+
+
+
